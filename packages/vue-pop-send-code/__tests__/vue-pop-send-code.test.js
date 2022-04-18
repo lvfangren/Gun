d6d52c3b7d35c8685a77939c8210bd90e718cdb6
lvfangren <m18188267414@163.com>
2022-04-18 23:24:52 +0800

    perf: change something

--- packages/vue-pop-send-code/__tests__/vue-pop-send-code.test.js
+++ packages/vue-pop-send-code/__tests__/vue-pop-send-code.test.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const vuePopSendCode = require('..');
+const vuePopSendCode = require('./lib/vue-pop-send-code.js');
 
 describe('vue-pop-send-code', () => {
     it('needs tests');
 });
+
+describe('我就随便测试下', () => {
+    console.log('sacs');
+})
